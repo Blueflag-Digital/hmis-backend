d33bff0138160d5839eea4fd2aa0343c735fe757
daveomosh97 <daveomosh97@gmail.com>
2024-06-15 21:29:35 +0300

    organized

--- routes/api.php
+++ routes/api.php
@@ -7,42 +7,49 @@ use App\Http\Controllers\PhoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'store');
+    Route::post('login-user', 'loginUser');
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('patients')->group(function () {
+        Route::post('/', [PatientController::class, 'index']);
+        Route::post('/add', [PatientController::class, 'store']);
+        Route::post('/show', [PatientController::class, 'show']);
+    });
 
     Route::controller(AuthController::class)->group(function () {
-        Route::post('register', 'store');
-        Route::post('login-user', 'loginUser');
+        Route::post('logout-user', 'logoutUser');
     });
 
-    Route::middleware('auth:sanctum')->group(function () {
-
-        Route::get('/user', function (Request $request) { // Get User
-            return $request->user();
-        });
-
-        Route::controller(AuthController::class)->group(function () {
-            Route::post('logout-user', 'logoutUser');
-
-            //patients routes
-            Route::prefix('patients')->group(function () {
-                Route::controller(PatientController::class)->group(function () {
-                    Route::post('/', 'index');
-                    Route::post('add', 'store');
-                    Route::post('/patient/{id}', 'show');
-                });
-            });
-
-            //people routes
-            Route::prefix('people')->group(function () {
-                Route::post('people/{person_id}/phones', [PhoneController::class, 'index']);
-                Route::post('people/{person_id}/phones', [PhoneController::class, 'store']);
-            });
-            //cities routes
-            Route::prefix('cities')->group(function () {
-                Route::controller(CityController::class)->group(function () {
-                    Route::post('/', 'index');
-                });
-            });
+    Route::prefix('cities')->group(function () {
+        Route::controller(CityController::class)->group(function () {
+            Route::post('/', 'index');
         });
     });
+
+    // People routes
+    // Route::prefix('people')->group(function () {
+    //     Route::post('people/{person_id}/phones', [PhoneController::class, 'index']);
+    //     Route::post('people/{person_id}/phones', [PhoneController::class, 'store']);
+    // });
+    // //cities routes
+    // Route::prefix('cities')->group(function () {
+    //     Route::controller(CityController::class)->group(function () {
+    //         Route::post('/', 'index');
+    //     });
+    // });
+
+
+
+
 });
