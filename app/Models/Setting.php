f6a0b4740d386ef42da2a0714cf525cb8ef60128
jacodhis <odhiambojack536@gmail.com>
2024-07-26 18:21:10 +0300

    added settings

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -14,6 +14,7 @@ class Setting extends Model
     public function settingsData(){
         return [
             'id'=> $this->id,
+            'slug'=> $this->slug,
             'name' => $this->name,
             'description'=> $this->description,
             'value'=> $this->value
--- config/app_settings.php
+++ config/app_settings.php
@@ -41,4 +41,12 @@ return [
         'created_at' => now(),
         'updated_at' => now(),
     ],
+     [
+        'name' => 'Reset Password',
+        'slug' => \Illuminate\Support\Str::slug('Reset Password'),
+        'value' => false,
+        'description' => 'This Reset Password setting requires users to verify their identity using two-factor authentication, providing an additional layer of security beyond the standard username and password. This is particularly important for protecting sensitive patient data and maintaining system integrity.',
+        'created_at' => now(),
+        'updated_at' => now(),
+     ],
 ];
