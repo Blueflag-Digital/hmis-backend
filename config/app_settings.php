d5bedf3c3d7ed4ded1a9373a7a9a96c005f9b9eb
daveomosh97 <daveomosh97@gmail.com>
2024-09-11 00:19:25 +0300

    changes

--- config/app_settings.php
+++ config/app_settings.php
@@ -5,7 +5,7 @@ return [
         'name' => 'Enable Suppliers Menu',
         'slug' => \Illuminate\Support\Str::slug('enable-suppliers-menu'),
         'value' => false,
-        'description' => 'This setting enables the patient portal, allowing patients to access their medical records, schedule appointments, and communicate with their healthcare providers online. It is crucial to provide secure access and ensure that all patient data is protected in compliance with HIPAA regulations.',
+        'description' => 'This setting turns on the supllier module and all its attachements/where needed',
         'created_at' => now(),
         'updated_at' => now(),
     ],
@@ -16,5 +16,5 @@ return [
         'description' => 'This Reset Password setting requires users to verify their identity using two-factor authentication, providing an additional layer of security beyond the standard username and password. This is particularly important for protecting sensitive patient data and maintaining system integrity.',
         'created_at' => now(),
         'updated_at' => now(),
-     ],
+    ],
 ];
